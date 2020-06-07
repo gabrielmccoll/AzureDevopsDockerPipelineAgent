6ed7f60bc8974bee9390fbdb235441ff8e421c69
gabriel <gabriel@cloudconfusion.co.uk>
2020-06-07 12:34:23 +0100

    added the run commands for swarm and also removed the CI trigger for the yaml builds

---
+++ DockerSwarmRun.ps1
@@ -0,0 +1,7 @@
+#https://docs.microsoft.com/en-us/virtualization/windowscontainers/manage-containers/swarm-mode
+#ipconfig
+#make sure on windows containers
+docker swarm init --advertise-addr=192.168.1.221 --listen-addr 192.168.1.221:2377 
+
+# Deploy a service to the swarm
+C:\> docker service create --name=ADOAgent --endpoint-mode dnsrr -e AZP_URL=<Azure DevOps instance> -e AZP_TOKEN=<PAT token> -e AZP_AGENT_NAME=DockerContainerAgent  gabrielmccoll/windowsadoagent:194
--- runcommand.ps1
+++ runcommand.ps1
@@ -1,1 +1,2 @@
+#https://docs.microsoft.com/en-us/azure/devops/pipelines/agents/docker?view=azure-devops
 docker run -e AZP_URL=<Azure DevOps instance> -e AZP_TOKEN=<PAT token> -e AZP_AGENT_NAME=mydockeragent dockeragent:latest
