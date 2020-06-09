38339f59f18944b21de882c5ec40275aa2fa67d2
gabriel <gabriel@cloudconfusion.co.uk>
2020-06-09 21:55:01 +0100

    removed need for specific tagged version

--- DockerSwarmRun.ps1
+++ DockerSwarmRun.ps1
@@ -4,4 +4,4 @@
 docker swarm init --advertise-addr=192.168.1.221 --listen-addr 192.168.1.221:2377 
 
 # Deploy a service to the swarm
-docker service create --name=ADOAgent --endpoint-mode dnsrr -e AZP_URL=<Azure DevOps instance> -e AZP_TOKEN=<PAT token> -e AZP_AGENT_NAME=DockerContainerAgent  gabrielmccoll/windowsadoagent:194
+docker service create --name=ADOAgent --endpoint-mode dnsrr -e AZP_URL=<Azure DevOps instance> -e AZP_TOKEN=<PAT token> -e AZP_AGENT_NAME=DockerContainerAgent  gabrielmccoll/windowsadoagent
